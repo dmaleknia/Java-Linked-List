d866bd2be4138f568bb0cb6cc941552aa72562b3
00Vantablack <00vantablack@00Vantablacks-MacBook-Pro.local>
2024-06-08 13:51:43 -0700

    Put tests in main method

--- linkedlist.java
+++ linkedlist.java
@@ -48,36 +48,4 @@ public class LinkedList {
         // Return null to indicate that the name was not found
         return null;
     }
-
-    public static void main(String[] args) {
-        // Create a new LinkedList object
-        LinkedList touristList = new LinkedList();
-
-        // Adding tourists 
-        touristList.addFirst("John", 123456, "London");
-        touristList.addFirst("Jane", 654321, "Paris");
-        touristList.addFirst("Bob", 111111, "New York");
-
-        // Finding tourists 
-        System.out.println("Destination of John: " + touristList.findNode("John"));
-        System.out.println("Destination of Jane: " + touristList.findNode("Jane"));
-        System.out.println("Destination of Bob: " + touristList.findNode("Bob"));
-
-        // Output the size of the linked list
-        System.out.println("Size of the linked list: " + touristList.getSize());
-
-        // Adding more tourists
-        touristList.addFirst("Alice", 222222, "Tokyo");
-        touristList.addFirst("Charlie", 333333, "Sydney");
-
-        // Finding more tourists
-        System.out.println("Destination of Alice: " + touristList.findNode("Alice"));
-        System.out.println("Destination of Charlie: " + touristList.findNode("Charlie"));
-
-        // Output the size of the linked list
-        System.out.println("Size of the linked list: " + touristList.getSize());
-
-        // Fun findNode to attempt to find a tourist that does not exist in the list. 
-        System.out.println("Destination of Dave: " + touristList.findNode("Dave"));
-    }
 }
