adcd36b1c4ffa9294472327d018c869bfeb4ca4f
00Vantablack <00vantablack@00Vantablacks-MacBook-Pro.local>
2024-06-10 13:45:26 -0700

    Nested Node class into LinkedList class

--- LinkedList.java
+++ LinkedList.java
@@ -1,4 +1,34 @@
 public class LinkedList {
+
+    // Node class definition
+    public static class Node {
+        // Data stored in the node
+        private Tourist data;
+        // Reference to the next node in the list
+        private Node next;
+
+        // Constructor to initialize a new node with data
+        public Node(Tourist data) {
+            this.data = data;
+            this.next = null;
+        }
+
+        // Getter for the data
+        public Tourist getData() {
+            return data;
+        }
+
+        // Getter for the next node
+        public Node getNext() {
+            return next;
+        }
+
+        // Setter for the next node
+        public void setNext(Node next) {
+            this.next = next;
+        }
+    }
+
     // Head node of the linked list
     private Node head;
     // Size of the linked list
